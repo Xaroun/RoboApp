2dd7d7e7e33a722c71a251ec7c153cacb1b56c10
Xaroun <mateusz1003@gmail.com>
2016-03-29 17:01:08 +0200

    add login activity

---
+++ app/src/main/java/roboniania/com/roboniania_android/LoginActivity.java
@@ -0,0 +1,32 @@
+package roboniania.com.roboniania_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Initialize components
+        loginBtn = (Button) findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+}
--- app/src/main/java/roboniania/com/roboniania_android/MainActivity.java
+++ app/src/main/java/roboniania/com/roboniania_android/MainActivity.java
@@ -1,13 +1,50 @@
 package roboniania.com.roboniania_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LOGIN_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isAuthenticated()) {
+            startHomeActivity();
+        } else {
+            startLoginActivity();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == LOGIN_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                startHomeActivity();
+            }
+            if (resultCode == RESULT_CANCELED) {
+                Log.e("ERR", "Error during logging in.");
+            }
+        }
+    }
+
+    private boolean isAuthenticated() {
+        return false;
+    }
+
+    private void startHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void startLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivityForResult(intent, LOGIN_REQUEST);
     }
 }
