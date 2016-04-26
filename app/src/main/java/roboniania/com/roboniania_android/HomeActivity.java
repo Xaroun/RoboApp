edaaf8cf9966e741eb0c6073d7ed0c8f4610e874
Xaroun <mateusz1003@gmail.com>
2016-04-26 16:59:05 +0200

    add menu item

--- app/src/main/java/roboniania/com/roboniania_android/HomeActivity.java
+++ app/src/main/java/roboniania/com/roboniania_android/HomeActivity.java
@@ -1,7 +1,11 @@
 package roboniania.com.roboniania_android;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.add:
+                startFragmentPair();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void startFragmentPair() {
+
     }
 }
