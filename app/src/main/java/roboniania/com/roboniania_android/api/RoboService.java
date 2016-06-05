cbe9c5121bf5d3b992fa074a640fcbb3b6148c2b
Xaroun <mateusz1003@gmail.com>
2016-06-06 00:11:08 +0200

    add scrollview in few activities

--- app/src/main/java/roboniania/com/roboniania_android/api/RoboService.java
+++ app/src/main/java/roboniania/com/roboniania_android/api/RoboService.java
@@ -1,7 +1,7 @@
 package roboniania.com.roboniania_android.api;
 
 public interface RoboService {
-    public static final String ENDPOINT = "http://212.67.146.119:8080";
+    public static final String ENDPOINT = "http://192.168.2.6:8080";
     public static final String OAUTH = ENDPOINT + "/oauth2/token";
     public static final String ROBOTS_PAIR = ENDPOINT + "/robots";
     public static final String ROBOTS_LIST = ENDPOINT + "/accounts/robots";
