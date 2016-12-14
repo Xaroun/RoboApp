11ec4ff4ea8a5a6b055230190a0bc59f8a89cbd9
Xaroun <mateusz1003@gmail.com>
2016-12-14 19:10:15 +0100

    Fake playing

--- app/src/main/java/roboniania/com/roboniania_android/activities/GameActivity.java
+++ app/src/main/java/roboniania/com/roboniania_android/activities/GameActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +24,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -34,6 +36,7 @@ import java.net.Socket;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -54,13 +57,15 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     public static final String GAME_EXTRA_KEY = "game";
     private Toolbar toolbar;
     private SharedPreferenceStorage userLocalStorage;
-    private Button play;
+    private Button start, play, stop;
     private static final String TAG = GameActivity.class.getSimpleName();
     private Context context;
     private Game game;
     private String uuid = null;
     private List<NewRobot> robotsList;
     private ProgressDialog progress;
+    private String robotIp, gameCode;
+    private int port;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,8 +79,14 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         userLocalStorage = new SharedPreferenceStorage(this);
         context = getApplicationContext();
 
+        start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(this);
+
         play = (Button) findViewById(R.id.play);
         play.setOnClickListener(this);
+
+        stop = (Button) findViewById(R.id.stop);
+        stop.setOnClickListener(this);
 
         //SETTING UP TOOLBAR
         toolbar = (Toolbar) findViewById(R.id.app_bar);
@@ -90,6 +101,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     public void downloadRobotList(final String gameCode) {
 
+        this.gameCode = gameCode;
         Gson gson = new GsonBuilder().create();
 
         Retrofit retrofit = new Retrofit.Builder()
@@ -160,7 +172,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.play:
+            case R.id.start:
                 switch (game.getTitleId()) {
                     case R.string.label_tictac:
                         downloadRobotList("TIC_TAC_TOE");
@@ -175,8 +187,80 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                         break;
                 }
                 break;
+
+            case R.id.play:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startPlaying();
+                    }
+                }).start();
+                break;
+            case R.id.stop:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        stopPlaying();
+                    }
+                }).start();
+                break;
+
         }
 
+    }
+
+    private void stopPlaying() {
+        try {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    showStopDialog();
+                }
+            });
+
+            Socket client = new Socket();
+            client.connect(new InetSocketAddress(robotIp, port), 10000);
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+            out.writeUTF("STOP");
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            String response = in.readUTF();
+            System.out.println(response);
+            client.close();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    play.setVisibility(View.VISIBLE);
+                    play.setEnabled(true);
+                    stop.setVisibility(View.GONE);
+                    stop.setEnabled(false);
+                    Toast.makeText(context, R.string.stopped_game, Toast.LENGTH_SHORT).show();
+                    progress.dismiss();
+                }
+            });
+        } catch(Exception e) {
+            e.printStackTrace();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(context, R.string.cannot_connect, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void showStopDialog() {
+        progress = new ProgressDialog(GameActivity.this, R.style.AppTheme_Dialog);
+        progress.setTitle(R.string.stopping);
+        progress.setMessage("Stopping game");
+        progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progress.setProgressNumberFormat(null);
+        progress.setProgressPercentFormat(null);
+        progress.setIndeterminate(true);
+        progress.setCancelable(false);
+        progress.show();
     }
 
     private void showRobotsPopupList(final String gameCode) {
@@ -210,21 +294,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         String selectedRobotIp = arrayAdapter.getItem(which);
-                        launchProgressDialog(selectedRobotIp, gameCode);
-
-//                        AlertDialog.Builder builderInner = new AlertDialog.Builder(
-//                                GameActivity.this);
-
-//                        builderInner.setMessage(selectedRobotIp);
-//                        builderInner.setTitle("Your Selected Item is");
-//                        builderInner.setPositiveButton("Ok",
-//                                new DialogInterface.OnClickListener() {
-//                                    @Override
-//                                    public void onClick(DialogInterface dialog, int which) {
-//                                        dialog.dismiss();
-//                                    }
-//                                });
-//                        builderInner.show();
+                        robotIp = selectedRobotIp;
+                        launchProgressDialog(robotIp, gameCode);
                     }
                 });
         builderSingle.show();
@@ -247,19 +318,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             public void run() {
                 try {
                     // CONNECTING TO ROBOT
-//                    String url = "192.168.2.4";
-                    int port = 3456;
-                    connectToRobot(robotIp, port, gameCode);
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if(progress.isShowing()) {
-                                Toast.makeText(context, R.string.started_game, Toast.LENGTH_SHORT).show();
-                            }
-
-                        }
-                    });
+                    port = 3456;
+                    startPolling(robotIp, port, gameCode);
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -269,67 +329,90 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                         public void run() {
                             if(progress.isShowing()) {
                                 Toast.makeText(context, R.string.cannot_connect, Toast.LENGTH_SHORT).show();
+                                progress.dismiss();
                             }
 
                         }
                     });
                 }
-                progress.dismiss();
             }
         }).start();
     }
 
-    private void connectToRobot(String robotIp, int port, String gameCode) throws IOException{
-        System.out.println("Connecting to " + robotIp + " on port " + port);
-        Socket client = new Socket();
-        client.connect(new InetSocketAddress(robotIp, port), 10000);
-        System.out.println("Sending " + gameCode + " request" + client.getRemoteSocketAddress());
-        OutputStream outToServer = client.getOutputStream();
-        DataOutputStream out = new DataOutputStream(outToServer);
-        out.writeUTF(gameCode);
-        client.close();
+    private void startPolling(String robotIp, int port, String gameCode) throws IOException, InterruptedException {
+        boolean flag = true;
+        while(flag) {
+            Socket client = new Socket();
+            client.connect(new InetSocketAddress(robotIp, port), 10000);
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+            out.writeUTF(gameCode);
+
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            String response = in.readUTF();
+            switch (response) {
+                case "DOWNLOADING":
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progress.setTitle(R.string.download);
+                            progress.setMessage("Downloading game.. ");
+                        }
+                    });
+
+                    break;
+                case "READY":
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progress.dismiss();
+                            Toast.makeText(context, R.string.game_is_ready, Toast.LENGTH_SHORT).show();
+                            start.setVisibility(View.GONE);
+                            start.setEnabled(false);
+                            play.setVisibility(View.VISIBLE);
+                            play.setEnabled(true);
+                        }
+                    });
+                    flag = false;
+                    break;
+            }
+
+            client.close();
+        }
+
     }
 
-    private void startPlaying(String game) {
-//        System.out.println(uuid);
+    private void startPlaying() {
+        try {
+            Socket client = new Socket();
+            client.connect(new InetSocketAddress(robotIp, port), 10000);
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+            out.writeUTF(gameCode);
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            String response = in.readUTF();
+            System.out.println(response);
+            client.close();
 
-        if(uuid != null) {
-            Gson gson = new GsonBuilder().create();
-
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(RoboService.ENDPOINT)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-
-            RoboService roboService = retrofit.create(RoboService.class);
-
-            Call<Void> call = roboService.startPlaying(uuid, game);
-
-            call.enqueue(new Callback<Void>() {
+            runOnUiThread(new Runnable() {
                 @Override
-                public void onResponse(Call<Void> call, Response<Void> response) {
-                    int statusCode = response.code();
-                    if (response.isSuccessful()) {
-
-                        Log.d(TAG, "ROBOT IS PLAYING");
-                        Toast.makeText(context, "Robot just started game.", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, Integer.toString(statusCode));
-
-                    } else {
-                        Log.d(TAG, Integer.toString(statusCode));
-                        Log.d(TAG, "ERROR?");
-                        Toast.makeText(context, "ERROR", Toast.LENGTH_SHORT).show();
-                    }
+                public void run() {
+                    play.setVisibility(View.GONE);
+                    play.setEnabled(false);
+                    stop.setVisibility(View.VISIBLE);
+                    stop.setEnabled(true);
+                    Toast.makeText(context, R.string.started_game, Toast.LENGTH_SHORT).show();
                 }
-
-                @Override
-                public void onFailure(Call<Void> call, Throwable t) {
-                    Toast.makeText(context, R.string.check_connection, Toast.LENGTH_SHORT).show();
-                }
-
             });
-        } else {
-            Toast.makeText(context, R.string.no_robots, Toast.LENGTH_SHORT).show();
+        } catch(Exception e) {
+            e.printStackTrace();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(context, R.string.cannot_connect, Toast.LENGTH_SHORT).show();
+                }
+            });
         }
 
     }
